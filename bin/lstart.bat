508f04da6f56366890e0280edb9dff8c9fc0d606
Veronica Iovinella <vero.neeja@gmail.com>
2017-04-07 19:47:53 +0200

    add check for already existing lab

--- bin/lstart.bat
+++ bin/lstart.bat
@@ -2,4 +2,8 @@
 
 python %NETKIT_HOME%\python\check.py
 
-FOR /F "tokens=*" %%a in ('python %NETKIT_HOME%\python\lstart.py %cd%\') do %%a
+FOR /F "tokens=*" %%a in ('python %NETKIT_HOME%\python\folder_hash.py %cd%\') do SET VAR1=%NETKIT_HOME%\temp\%%a_machines
+
+if not exist %VAR1% (
+    FOR /F "tokens=*" %%a in ('python %NETKIT_HOME%\python\lstart.py %cd%\') do %%a
+) else (FOR /f "delims=" %%b in (%VAR1%) do docker start %%b & FOR /F "tokens=*" %%c in ('python %NETKIT_HOME%\python\lstart.py --execbash %cd%\') do %%c)
--- bin/python/lstart.py
+++ bin/python/lstart.py
@@ -7,6 +7,7 @@ nc.DEBUG = False
 
 parser = argparse.ArgumentParser(description='Create and start a Netkit Lab.')
 parser.add_argument('path')
+parser.add_argument("--execbash", action="store_true")
 
 args = parser.parse_args()
 
@@ -16,7 +17,8 @@ args = parser.parse_args()
 (commands, startup_commands, exec_commands) = nc.create_commands(machines, links, options, metadata, args.path)
 
 # create lab
-cr.lab_create(commands, startup_commands)
+if not args.execbash:
+    cr.lab_create(commands, startup_commands)
 
 COMMAND_LAUNCHER = "bash -c '"
 COMMAND_LAUNCHER_END = "'"
