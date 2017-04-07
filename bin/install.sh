d77586833114baeb344f9ec38ab67d7fc206dc77
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2017-04-07 21:02:18 +0200

    fix for install and start on unix

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-
 sudo true
+
+sudo chmod 755 $NETKIT_HOME/lstart.sh
+sudo chmod 755 $NETKIT_HOME/lclean.sh
+sudo chmod 755 $NETKIT_HOME/lcrash.sh
+sudo chmod 755 $NETKIT_HOME/lhalt.sh
+sudo chmod 755 $NETKIT_HOME/linfo.sh
+
 sudo docker rmi -f netkit
 sudo docker build -t netkit $NETKIT_HOME
--- bin/lstart.sh
+++ bin/lstart.sh
@@ -7,6 +7,13 @@ M=_machines
 
 sudo true
 python $NETKIT_HOME/python/folder_hash.py "$PWD/" | 
-while read in; do ( if "$NETKIT_HOME/temp/$in$M"; python $NETKIT_HOME/python/lstart.py "$PWD/" then 
-sudo docker start `cat "$NETKIT_HOME/temp/$in$M"`; (python $NETKIT_HOME/python/lstart.py --execbash "$PWD/" | while read in; do (sudo xterm -e "$in" &);done )
-else (python $NETKIT_HOME/python/lstart.py "$PWD/" | while read in; do (sudo xterm -e "$in" &); done ) fi ); done
+while read in; do ( 
+    if [ -f "$NETKIT_HOME/temp/$in$M" ]; 
+    then 
+        sudo docker start `cat "$NETKIT_HOME/temp/$in$M"`; 
+            python $NETKIT_HOME/python/lstart.py --execbash "$PWD/" | 
+            (while read in; do (sudo xterm -e "$in" &); done)
+    else 
+        python $NETKIT_HOME/python/lstart.py "$PWD/" | while read in; do (sudo xterm -e "$in" &); done  
+    fi ); 
+done
