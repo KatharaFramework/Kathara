1c7d304474076a9fc0b0e76c5a18cfd8819d42ce
Veronica Iovinella <vero.neeja@gmail.com>
2017-04-13 19:13:27 +0200

    add simple startup file creation for vstart

--- bin/python/vstart.py
+++ bin/python/vstart.py
@@ -13,20 +13,25 @@ parser = argparse.ArgumentParser(description='Create and start a Netkit Machine.
 parser.add_argument('path')
 parser.add_argument('machine_name')
 parser.add_argument('eths', nargs='*')
+parser.add_argument('-e', '--exe', nargs='*')
 
 args = parser.parse_args()
 
+#creating lab.conf for single machine
 conf_line_template = '{machine_name}[{argument}]={value}\n'
-create_machine_conf = []
 
+confp = open(args.path + 'lab.conf', 'w')
 for eth in args.eths:
     match = re.search(r'eth([0-9]+)=([A-Z]+)', eth)
     if match:
         repls = ('{machine_name}', args.machine_name), ('{argument}', match.group(1)), ('{value}', match.group(2))
-        create_machine_conf.append(u.replace_multiple_items(repls, conf_line_template))
-
-confp = open(args.path + 'lab.conf', 'w')
-for line in create_machine_conf:
-    confp.write(line)
+        confp.write(u.replace_multiple_items(repls, conf_line_template))
 
 confp.close()
+
+#creating {machine_name}.startup file
+startupp = open (args.path + args.machine_name + '.startup', 'w')
+for command in args.exe:
+    startupp.write(command + "\n")
+
+startupp.close()
