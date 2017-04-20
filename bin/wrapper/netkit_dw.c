5ec63a4635beffecdb99aa3c7abf96a839ac922f
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2017-04-20 12:52:50 +0200

    wrapper now allows instead of forbidding

--- bin/wrapper/netkit_dw.c
+++ bin/wrapper/netkit_dw.c
@@ -4,7 +4,12 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define MAX_CMD_LEN 1000
+#define MAX_CMD_LEN 1500
+
+char* allowed_words_1 [] = { "run", "exec", "kill", "rm", "stop", "start", "rmi", "connect", "create" };
+char* allowed_words_2 [] = { "-ti", "-tid", "-it", "-itd", "-dit", "-dti", "--privileged=true", "--name", "--hostname=", "--network=", "-m=", "-f", "-e", "-d" };
+#define ALLOWED_WORDS_1_LEN 9
+#define ALLOWED_WORDS_2_LEN 14
 
 void check_overflow(int count) 
 {
@@ -24,17 +29,50 @@ void check_mount_option(char* p)
     }
 }
 
-void check_invalid_cp(char* p, int is_cp, int current_arg, int total_args) 
+int is_path_in_container(char* p)
 {
-    if(is_cp==1 && current_arg < total_args-1 && strstr(p, ":") != NULL) 
+    return strstr(p, ":") != NULL;
+}
+
+int is_not_last_arg(int current_arg, int total_args)
+{
+    return current_arg < total_args-1;
+}
+
+int is_valid_cp(char* p, int current_arg, int total_args) 
+{
+    if(is_not_last_arg(current_arg, total_args) && is_path_in_container(p)) 
     {
         fprintf(stderr, "cp from container to host is not allowed\n");
-        exit(EXIT_FAILURE); 
+        return 0;
     }
+    return 1;
+}
+
+int contains(char* haystack[], char* needle, int len)
+{
+    
+    for(int i = 0; i < len; ++i)
+    {
+        if(strncmp(haystack[i], needle, strlen(haystack[i]))==0) //ensures that words like --hostname=anything are accepted
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int is_allowed_word(char* p, char** allowed_words, int len, int strict)
+{
+    if(contains(allowed_words, p, len) || (strncmp("-", p, 1)!=0) && !strict)
+        return 1;
+    return 0;
 }
 
 int main(int argc, char *argv[]) 
 {
+    typedef enum {INITIAL, CP1, CP2, CPOK, NETWORK, OK} state;
+    state current_state = INITIAL;
 
     char cmd[MAX_CMD_LEN] = "";
     char **p;
@@ -50,23 +88,59 @@ int main(int argc, char *argv[])
         int char_count = 7 + strlen(argv[1]);
         check_overflow(char_count);
         strcat(cmd, argv[1]);
-        int is_cp = 0;
         if(strncmp(argv[1], "cp", 2)==0) 
-            is_cp = 1;
+            current_state = CP1;
+        else if(is_allowed_word(argv[1], allowed_words_1, ALLOWED_WORDS_1_LEN, 1))
+            current_state = OK;
+        else if(strncmp(argv[1], "network", 7)==0) 
+            current_state = NETWORK;
+        else 
+        {   //current_stete = PIT;
+            fprintf(stderr, "Usage: netkit_dw [options] command\n");
+            exit(EXIT_FAILURE);
+        }
         int current_arg = 2;
         for(p = &argv[2]; *p; p++)
         {
-            check_invalid_cp(*p, is_cp, current_arg, argc); 
-            check_mount_option(*p);
+            if(current_state==CP1)
+                if (is_valid_cp(*p, current_arg, argc))
+                    current_state = CP2;
+                else //current_stete = PIT;
+                    exit(EXIT_FAILURE);
+            if(current_state==CP2)
+                if (is_valid_cp(*p, current_arg, argc))
+                    current_state = CPOK; // there cannot be other arguments if we are here
+                else //current_stete = PIT;
+                    exit(EXIT_FAILURE);
+            if(current_state==NETWORK)
+                if (is_allowed_word(*p, allowed_words_1, ALLOWED_WORDS_1_LEN, 1))
+                    current_state = OK;
+                else 
+                {   //current_stete = PIT;
+                    fprintf(stderr, "Parameter %i not allowed (1)\n", current_arg);
+                    exit(EXIT_FAILURE);
+                }
+            if(current_state==OK)
+                if (is_allowed_word(*p, allowed_words_2, ALLOWED_WORDS_2_LEN, 0))
+                    current_state = OK;
+                else 
+                {   //current_stete = PIT;
+                    fprintf(stderr, "Parameter %i not allowed (2)\n", current_arg);
+                    exit(EXIT_FAILURE);
+                }
+            check_mount_option(*p); // redundant check for -v parameter
             char_count += strlen(*p) + 1;
             check_overflow(char_count);
             strcat(cmd, " ");
             strcat(cmd, *p);
             current_arg++;
         }
-	
-	setuid(0);
-        system(cmd);
+        
+        if(current_state==OK || current_state==CPOK)
+        {
+            setuid(0);
+            system(cmd);
+        }
     }
 
     return 0;
