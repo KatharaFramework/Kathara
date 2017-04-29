d15bd4ee2fe6c428df228e432715fda9ad88817c
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2017-04-29 17:59:04 +0200

    removed deprecated args

--- bin/vclean.bat
+++ bin/vclean.bat
@@ -4,26 +4,7 @@ IF [%1]==[] GOTO USAGE
 
 for %%a in (%*) do SET NETKIT_LASTARG=%%a
 
-SET FORCE_RT=""
-:loop
-IF NOT "%1"=="" (
-    IF "%1"=="--remove-tunnels" (
-        SET FORCE_RT="-f"
-        SHIFT
-    )
-    IF "%1"=="-T" (
-        SET FORCE_RT="-f"
-        SHIFT
-    )
-    IF "%1"=="--clean-all" (
-        SET FORCE_RT="-f"
-        SHIFT
-    )
-    SHIFT
-    GOTO :loop
-)
-
-CALL %NETKIT_HOME%\lclean %FORCE_RT% -d %NETKIT_HOME%\temp/labs/netkit_nt_%NETKIT_LASTARG%
+CALL %NETKIT_HOME%\lclean -d %NETKIT_HOME%\temp/labs/netkit_nt_%NETKIT_LASTARG%
 
 GOTO END
 
