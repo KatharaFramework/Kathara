247ca284cd08ae0da26cad30d3672e33ca73eb4b
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2017-05-01 21:53:50 +0200

    fixes to l commands

--- bin/lclean.bat
+++ bin/lclean.bat
@@ -5,15 +5,19 @@ IF ERRORLEVEL 1 GOTO END
 
 SET NETKIT_ALL=1
 SET NETKIT_APP=%1
+SET NETKIT_APP_PREV=%0
 FOR %%p in (%*) DO (
     SET NETKIT_APP=%%p
     IF "%%p" == "--list" ( 
         SET NETKIT_LIST=1
     )
-    IF /i NOT "%NETKIT_APP:~0,1%" == "-" (
-        SET NETKIT_ALL=0 
-        docker rm -f netkit_nt_%%p
+    IF NOT "%NETKIT_APP:~0,1%" == "-" (
+        IF NOT "%NETKIT_APP_PREV%" == "-d" (
+            SET NETKIT_ALL=0 
+            docker rm -f netkit_nt_%%p
+        )
     )
+    SET NETKIT_APP_PREV=%%p
 )
 
 FOR /F "tokens=*" %%a in ('python %NETKIT_HOME%\python\folder_hash.py "%cd%/" %*') DO SET VAR1=%NETKIT_HOME%\temp\%%a_machines
--- bin/lhalt.bat
+++ bin/lhalt.bat
@@ -4,17 +4,20 @@ python %NETKIT_HOME%\python\check.py "%cd%/" %*
 IF ERRORLEVEL 1 GOTO END
 
 SET NETKIT_LIST=0
-SET NETKIT_ALL=1
 SET NETKIT_APP=%1
+SET NETKIT_APP_PREV=%0
 FOR %%p in (%*) DO (
     SET NETKIT_APP=%%p
     IF "%%p" == "--list" ( 
         SET NETKIT_LIST=1
     )
-    IF /i NOT "%NETKIT_APP:~0,1%" == "-" (
-        SET NETKIT_ALL=0 
-        docker stop netkit_nt_%%p
+    IF NOT "%NETKIT_APP:~0,1%" == "-" (
+        IF NOT "%NETKIT_APP_PREV%" == "-d" (
+            SET NETKIT_ALL=0 
+            docker stop netkit_nt_%%p
+        )
     )
+    SET NETKIT_APP_PREV=%%p
 )
 
 IF "%NETKIT_ALL%" == "1" (
