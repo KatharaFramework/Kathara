b3d3d9be14a8f199ba1be0b8ab141584184376b5
Veronica Iovinella <vero.neeja@gmail.com>
2018-08-01 17:49:07 +0200

    fix lwipe on windows

--- bin/lwipe.bat
+++ bin/lwipe.bat
@@ -5,11 +5,11 @@ FOR /F "tokens=*" %%a in ('python %NETKIT_HOME%\python\adapter.py') DO SET ADAPT
 echo "This command will wipe containers and cache associated with Kathara"
 
 for /F "tokens=*" %%i in ('%ADAPTER_BIN% ps -a') do (
-	for /F "tokens=*" %%d in ('%NETKIT_HOME%\python\rm_node_name_from_ps.py %%i') do %ADAPTER_BIN% %%d
+	for /F "tokens=*" %%d in ('python %NETKIT_HOME%\python\rm_node_name_from_ps.py %%i') do %ADAPTER_BIN% %%d
 )
 
 for /F "tokens=*" %%n in ('%ADAPTER_BIN% network list') do (
-	for /F "tokens=*" %%e in ('%NETKIT_HOME%\python\rm_network_name_from_list.py %%n') do %ADAPTER_BIN% %%e
+	for /F "tokens=*" %%e in ('python %NETKIT_HOME%\python\rm_network_name_from_list.py %%n') do %ADAPTER_BIN% %%e
 )
 
 set folder=%NETKIT_HOME%\temp\
