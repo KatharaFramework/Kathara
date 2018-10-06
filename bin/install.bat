b466e15d11a2d6c86b3c4322c088c2464ed7e1c9
Gaetano Bonofiglio <Kidel@users.noreply.github.com>
2018-10-06 20:13:32 +0200

    add option to install p4

--- bin/install.bat
+++ bin/install.bat
@@ -2,5 +2,16 @@
 
 ECHO win_bin=docker > %NETKIT_HOME%\..\config
 
+SET SKIP_P4=1
+SET NETKIT_APP=%1
+FOR %%p in (%*) DO (
+    SET NETKIT_APP=%%p
+    IF "%%p" == "--p4" ( 
+        SET SKIP_P4=0
+    )
+)
+
 docker pull kathara/netkit_base
-docker pull kathara/p4
+IF "%SKIP_P4%" == "0" (
+  docker pull kathara/p4
+)
