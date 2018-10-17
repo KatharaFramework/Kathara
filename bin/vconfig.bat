92bafb1ca1930cff155c71a3a93d2b78ff19d5bd
Gaetano Bonofiglio <Kidel@users.noreply.github.com>
2018-10-17 18:01:10 +0200

    add brctl patch to vconfig.bat

--- bin/vconfig.bat
+++ bin/vconfig.bat
@@ -10,6 +10,7 @@ FOR %%p in (%*) DO (
     SET NETKIT_APP=%%p
     IF /i NOT "%NETKIT_APP:~0,1%" == "-" (
         CALL %ADAPTER_BIN% network create netkit_nt_%%p
+        CALL %NETKIT_HOME%/brctl_config netkit_nt_%%p
         CALL %ADAPTER_BIN% network connect netkit_nt_%%p netkit_nt_%NETKIT_LASTARG%
     )
 )
