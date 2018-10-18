87d08f1494a9069d2f0e0883ca90054f4704682d
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2018-10-18 19:31:35 +0200

    fix permission in install and add check

---
+++ bin/check.bat
@@ -0,0 +1,25 @@
+@echo off
+
+ECHO Docker version is
+docker --version
+echo.
+
+ECHO Trying to run Docker Hello World
+docker run --rm hello-world
+echo.
+
+ECHO Python version is
+python -V
+echo.
+
+ECHO NETKIT_HOME is
+ECHO %NETKIT_HOME%
+echo.
+
+ECHO Config file contents are
+type %NETKIT_HOME%\..\config
+echo.
+
+ECHO Netkit temp folder contains
+dir %NETKIT_HOME%\temp\
+echo.
