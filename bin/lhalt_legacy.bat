856b4ab557f8290c9b429e5f6e9a96e8b2b37ae9
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2018-10-30 16:15:02 +0100

    fix for legacy commands

--- bin/lhalt_legacy.bat
+++ bin/lhalt_legacy.bat
@@ -1,10 +1,12 @@
 @echo off
+setlocal enableDelayedExpansion
 
 FOR /F "tokens=*" %%a in ('python %NETKIT_HOME%\python\adapter.py') DO SET ADAPTER_BIN=%%a
 
 python %NETKIT_HOME%\python\check.py "%cd%/" %*
 IF ERRORLEVEL 1 GOTO END
 
+SET NETKIT_ALL=1
 SET NETKIT_LIST=0
 SET NETKIT_APP=%1
 SET NETKIT_APP_PREV=%0
@@ -13,16 +15,16 @@ FOR %%p in (%*) DO (
     IF "%%p" == "--list" ( 
         SET NETKIT_LIST=1
     )
-    IF NOT "%NETKIT_APP:~0,1%" == "-" (
-        IF NOT "%NETKIT_APP_PREV%" == "-d" (
+    IF NOT "!NETKIT_APP:~0,1!" == "-" (
+        IF NOT "!NETKIT_APP_PREV!" == "-d" (
             SET NETKIT_ALL=0 
-            %ADAPTER_BIN% stop netkit_nt_%%p
+            %ADAPTER_BIN% kill netkit_nt_%%p
         )
     )
     SET NETKIT_APP_PREV=%%p
 )
 
-IF "%NETKIT_ALL%" == "1" (
+IF "!NETKIT_ALL!" == "1" (
     ECHO "Containers will be stopped (gracefully) but not deleted"
     FOR /F "tokens=*" %%a in ('python %NETKIT_HOME%\python\folder_hash.py "%cd%/" %*') DO SET VAR1=%NETKIT_HOME%\temp\%%a_machines
     IF exist %VAR1% (
@@ -30,6 +32,6 @@ IF "%NETKIT_ALL%" == "1" (
     )
 )
 
-IF "%NETKIT_LIST%" == "1" %ADAPTER_BIN% stats --no-stream & %ADAPTER_BIN% network list
+IF "!NETKIT_LIST!" == "1" %ADAPTER_BIN% stats --no-stream & %ADAPTER_BIN% network list
 
 :END
