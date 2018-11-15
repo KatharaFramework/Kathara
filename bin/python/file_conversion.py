6af107a9c054c860a47e8610fd94a340bbd77d76
Veronica Iovinella <vero.neeja@gmail.com>
2018-11-15 20:01:04 +0100

    handle non-text file

--- bin/python/file_conversion.py
+++ bin/python/file_conversion.py
@@ -5,9 +5,9 @@ import sys
 def win2linux(filename):
     c = open(filename).read()
     c = c[1:] if len(c) > 0 and ord(c[0]) == 0xfeff else c
-    try: #python3
-        open(filename, 'wb', encoding='utf-8').write(re.sub(r'\r', '', c))
-    except TypeError: #python2
+    try: #python3 
+        open(filename, 'wb').write(re.sub(r'\r', '', c).encode())
+    except: #python2
         open(filename, 'wb').write(re.sub(r'\r', '', c))
 
 
@@ -15,4 +15,7 @@ def win2linux_all_files_in_dir(some_dir):
     for dname, dirs, files in os.walk(some_dir):
         for fname in files:
             fpath = os.path.join(dname, fname)
-            win2linux(fpath)
+            try: 
+                win2linux(fpath)
+            except: # not a text file, probably
+                pass
