6539d58aec50d70fe3482a969b3a6620d9457b03
Veronica Iovinella <vero.neeja@gmail.com>
2018-11-15 17:12:52 +0100

    handle type error in python2

--- bin/python/file_conversion.py
+++ bin/python/file_conversion.py
@@ -5,10 +5,10 @@ import sys
 def win2linux(filename):
     c = open(filename).read()
     c = c[1:] if len(c) > 0 and ord(c[0]) == 0xfeff else c
-    """ try: #python3 """
+    try: #python3
     open(filename, 'wb', encoding='utf-8').write(re.sub(r'\r', '', c))
-    """     except: #python2
-        open(filename, 'wb').write(re.sub(r'\r', '', c)) """
+    except TypeError: #python2
+        open(filename, 'wb').write(re.sub(r'\r', '', c))
 
 
 def win2linux_all_files_in_dir(some_dir):
