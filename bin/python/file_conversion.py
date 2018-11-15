004a21c7c551667aa9be0352dcf9474337bb783f
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2018-11-15 22:22:47 +0100

    better file type handling

--- bin/python/file_conversion.py
+++ bin/python/file_conversion.py
@@ -2,16 +2,22 @@ import os
 import re
 import sys
 from utils import log 
+from utils import istextfile
 
 def win2linux(filename):
-    c = open(filename).read()
-    if len(c) <= 0:
-        return
-    if ('\r' in c):
-        try: #python3 
-            open(filename, 'wb').write(re.sub(r'\r', '', c).encode())
-        except: #python2
-            open(filename, 'wb').write(re.sub(r'\r', '', c))
+    c = None
+    istext = False
+    with open(filename) as f:
+        istext = istextfile(f)
+        if not istext:
+            return
+        else c = f.read()
+        if len(c) <= 0:
+            return
+    try: #python3 
+        open(filename, 'wb').write(re.sub(r'\r', '', c).encode())
+    except: #python2
+        open(filename, 'wb').write(re.sub(r'\r', '', c))
 
 
 def win2linux_all_files_in_dir(some_dir):
--- bin/python/utils.py
+++ bin/python/utils.py
@@ -8,6 +8,7 @@ try:
     import pwd
 except ImportError: #windows
     pass
+import sys
 
 non_ascii = r'[^\x00-\x7F]+'
 
@@ -59,3 +60,32 @@ def timestamp():
 def log(message, filepath=os.path.join(os.environ['NETKIT_HOME'], '..', 'logs.txt'), mode='a+'):
     with open(filepath, mode) as file:
         file.write(timestamp()  + ' ' + message + '\n')
+
+# A function that takes an integer in the 8-bit range and returns
+# a single-character byte object in py3 / a single-character string
+# in py2.
+#
+PY3 = sys.version_info[0] == 3
+int2byte = (lambda x: bytes((x,))) if PY3 else chr
+_text_characters = (
+        b''.join(int2byte(i) for i in range(32, 127)) +
+        b'\n\r\t\f\b')
+
+def istextfile(fileobj, blocksize=512):
+    """ Uses heuristics to guess whether the given file is text or binary,
+        by reading a single block of bytes from the file.
+        If more than 30% of the chars in the block are non-text, or there
+        are NUL ('\x00') bytes in the block, assume this is a binary file.
+    """
+    block = fileobj.read(blocksize)
+    if b'\x00' in block:
+        # Files with null bytes are binary
+        return False
+    elif not block:
+        # An empty file is considered a valid text file
+        return True
+
+    # Use translate's 'deletechars' argument to efficiently remove all
+    # occurrences of _text_characters from the block
+    nontext = block.translate(None, _text_characters)
+    return float(len(nontext)) / len(block) <= 0.30
