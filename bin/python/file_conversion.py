6077a7cf7f3b7f6a60b0b9205f487f6fef101b29
Veronica Iovinella <ver.iovinella@stud.uniroma3.it>
2018-11-15 21:12:36 +0100

    Handle non-text files in machine folders (#30) (#33)
    * add encoding utf-8 for python3
    
    * handle type error in python2
    
    * fix indentation
    
    * handle non-text file
    
    * add log function

--- bin/python/file_conversion.py
+++ bin/python/file_conversion.py
@@ -1,13 +1,23 @@
 import os
 import re
+import sys
+from utils import log 
 
 def win2linux(filename):
     c = open(filename).read()
     c = c[1:] if len(c) > 0 and ord(c[0]) == 0xfeff else c
-    open(filename, 'wb').write(re.sub(r'\r', '', c).encode('utf-8'))
+    try: #python3 
+        open(filename, 'wb').write(re.sub(r'\r', '', c).encode())
+    except: #python2
+        open(filename, 'wb').write(re.sub(r'\r', '', c))
+
 
 def win2linux_all_files_in_dir(some_dir):
     for dname, dirs, files in os.walk(some_dir):
         for fname in files:
             fpath = os.path.join(dname, fname)
-            win2linux(fpath)
+            try: 
+                win2linux(fpath)
+            except Exception as e: # not a text file, probably
+                log('Exception on ' + fpath)
+                log(str(e))
