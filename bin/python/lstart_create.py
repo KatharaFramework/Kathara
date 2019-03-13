0b231bda8155aec80dfc4dce1daaae0e8931903a
Gaetano Bonofiglio <Kidel@users.noreply.github.com>
2019-03-13 19:48:06 +0100

    Fix for command length limit (#43)
    This version solves the issue of the command length limit when the path of the lab or of $NETKIT_HOME was too long, or the lab was too big.

--- bin/python/lstart_create.py
+++ bin/python/lstart_create.py
@@ -1,6 +1,8 @@
 import command_utils as cu
 import netkit_commons as nc
+import utils as u
 import sys
+import os
 
 DEBUG = nc.DEBUG
 nc.DEBUG = False
@@ -10,12 +12,19 @@ PRINT = False
 def lab_create(commands, startup_commands):
     lab_create_command_string = ''
     for command in commands:
-        lab_create_command_string += command + nc.BASH_SEPARATOR
-    lab_create_command_string = lab_create_command_string[:len(lab_create_command_string)-2]
+        if nc.PLATFORM == nc.WINDOWS: lab_create_command_string += "CALL " + command + "\n"
+        else: lab_create_command_string += command + "\n"
     if PRINT: 
-        if nc.PLATFORM == nc.WINDOWS: sys.stderr.write(lab_create_command_string)
+        if nc.PLATFORM == nc.WINDOWS: sys.stderr.write(lab_create_command_string + "\n")
         else: print(lab_create_command_string)
-    else: cu.run_command_detatched(lab_create_command_string)
+    else:
+        if nc.PLATFORM == nc.WINDOWS: 
+            u.write_temp("@ECHO OFF \n\n" + lab_create_command_string, "last_lab_creation.bat", mode=nc.PLATFORM, file_mode="w")
+            cu.run_command_detatched('cd "' + u.get_temp_folder(mode=nc.PLATFORM) + '"' + nc.BASH_SEPARATOR + "last_lab_creation.bat")
+        else: 
+            u.write_temp("#!/bin/bash \n\n" + lab_create_command_string, "last_lab_creation.sh", mode=nc.PLATFORM, file_mode="w")
+            os.chmod(os.path.join(u.get_temp_folder(mode=nc.PLATFORM), "last_lab_creation.sh"), 0o755)
+            cu.run_command_detatched('cd "' + u.get_temp_folder(mode=nc.PLATFORM) + '"' + nc.BASH_SEPARATOR + "./last_lab_creation.sh")
 
     for startup_command in startup_commands:
         if PRINT: 
