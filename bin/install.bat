924178fa5aac91749936049198dd1434e444b736
Gaetano Bonofiglio <Kidel@users.noreply.github.com>
2019-03-15 00:42:06 +0100

    add pip install ipaddress

--- bin/install.bat
+++ bin/install.bat
@@ -4,15 +4,25 @@ ECHO win_bin=docker > %NETKIT_HOME%\..\config
 
 SET SKIP_P4=1
 SET NETKIT_APP=%1
+SET SKIP_PIP=0
 FOR %%p in (%*) DO (
     SET NETKIT_APP=%%p
     IF "%%p" == "--p4" ( 
         SET SKIP_P4=0
     )
+    IF "%%p" == "--nopip" ( 
+        SET SKIP_PIP=1
+    )
 )
 
+echo "Pulling images"
 docker pull kathara/netkit_base
 docker pull alpine
 IF "%SKIP_P4%" == "0" (
   docker pull kathara/p4
+)
+
+IF "%SKIP_PIP%" == "0" (
+  echo "Installing ipaddress using pip (to skip use --nopip)"
+  pip install ipaddress
 )
