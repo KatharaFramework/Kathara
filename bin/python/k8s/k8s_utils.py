7c66f0c6236b8dd8f676470f010a60d309dbb357
Mariano Scazzariello <marianoscazzariello@gmail.com>
2019-06-12 17:23:45 +0200

    Add support for remote cluster configuration using Kathara configuration file

--- bin/python/k8s/k8s_utils.py
+++ bin/python/k8s/k8s_utils.py
@@ -1,6 +1,7 @@
 import re
 
-from kubernetes import config
+import netkit_commons as nc
+from kubernetes import config, client
 
 
 def build_k8s_name(resource_name, prefix=""):
@@ -29,5 +30,19 @@ def get_extra_links_from_machine_options(machines, options):
 
 
 def load_kube_config():
-    # TODO: Add Custom API User configuration
-    config.load_kube_config()
+    try:
+        config.load_kube_config()           # Try to load configuration if Kathara is launched on a k8s master.
+    except Exception:                       # Not on a k8s master, load Kathara config to read remote cluster data.
+        # Try to read configuration. If this fails, throw an Exception.
+        try:
+            api_url = nc.kat_config['api_url']
+            token = nc.kat_config['token']
+        except Exception:
+            raise Exception("Cannot read Kubernetes configuration from Kathara.")
+
+        # Load the configuration and set it as default.
+        configuration = client.Configuration()
+        configuration.host = api_url
+        configuration.api_key = {"authorization": "Bearer " + token}
+
+        client.Configuration.set_default(configuration)
