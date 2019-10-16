9982bf1f3c902418175165fa1de9c7fa70d0c195
admin <admin@MacBook-Pro-di-admin.local>
2019-10-16 11:17:52 +0200

    OSX Make scripts fixed

--- scripts/OSX/darwin/uninstall.sh
+++ scripts/OSX/darwin/uninstall.sh
@@ -10,7 +10,7 @@ echo "Welcome to Kathara Uninstaller"
 echo "Kathara will be REMOVED!"
 
 while true; do
-    read -p "Do you wish to continue [Y/n]?" answer
+    read -p "Do you wish to continue [Y/n]? " answer
     [[ $answer == "y" || $answer == "Y" || $answer == "" ]] && break
     [[ $answer == "n" || $answer == "N" ]] && exit 0
     echo "Please answer with 'y' or 'n'"
@@ -22,36 +22,40 @@ echo "Application uninstalling process started"
 kathara wipe -a -f
 if [ $? -eq 0 ]
 then
-  echo "[1/4] [DONE] Successfully deleted all running Kathara machines"
+  echo "[1/5] [DONE] Successfully deleted all running Kathara machines"
 else
-  echo "[1/4] [ERROR] Could not delete all running Kathara machines" >&2
+  echo "[1/5] [ERROR] Could not delete all running Kathara machines" >&2
 fi
 
 # remove link to shorcut file
 find "/usr/local/bin/" -name "kathara" | xargs rm
 if [ $? -eq 0 ]
 then
-  echo "[2/4] [DONE] Successfully deleted shortcut links"
+  echo "[2/5] [DONE] Successfully deleted shortcut links"
 else
-  echo "[2/4] [ERROR] Could not delete shortcut links" >&2
+  echo "[2/5] [ERROR] Could not delete shortcut links" >&2
 fi
 
 # forget from pkgutil
 pkgutil --forget "org.kathara.kathara" > /dev/null 2>&1
 if [ $? -eq 0 ]
 then
-  echo "[3/4] [DONE] Successfully deleted application informations"
+  echo "[3/5] [DONE] Successfully deleted application informations"
 else
-  echo "[3/4] [ERROR] Could not delete application informations" >&2
+  echo "[3/5] [ERROR] Could not delete application informations" >&2
 fi
+
+__MANPAGES__
+echo "[4/5] [DONE] Successfully deleted man pages."
+
 
 # remove application source distribution
 [ -e "/Library/Kathara" ] && rm -rf "/Library/Kathara"
 if [ $? -eq 0 ]
 then
-  echo "[4/4] [DONE] Successfully deleted application"
+  echo "[5/5] [DONE] Successfully deleted application"
 else
-  echo "[4/4] [ERROR] Could not delete application" >&2
+  echo "[5/5] [ERROR] Could not delete application" >&2
 fi
 
 echo "Application uninstall process finished"
