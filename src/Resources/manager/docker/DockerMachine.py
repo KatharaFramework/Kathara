dba10e4473b32d5101a3af7bab160f8289545ee0
Mariano Scazzariello <marianoscazzariello@gmail.com>
2019-11-14 16:52:17 +0100

    Disabled icmp rate limit

--- src/Resources/manager/docker/DockerMachine.py
+++ src/Resources/manager/docker/DockerMachine.py
@@ -123,6 +123,7 @@ class DockerMachine(object):
         # Sysctl params to pass to the container creation
         sysctl_parameters = {RP_FILTER_NAMESPACE % x: 0 for x in ["all", "default", "lo"]}
         sysctl_parameters["net.ipv4.ip_forward"] = 1
+        sysctl_parameters["net.ipv4.icmp_ratelimit"] = 0
         # TODO: ipv6_forward not found?
         # sysctl_parameters["net.ipv6.ip_forward"] = 1
 
