bef7340e47a6550cbd1bd023c7cea0d9382c624c
Mariano Scazzariello <marianoscazzariello@gmail.com>
2020-03-24 17:38:53 +0100

    Fix k8s settings addon name

--- src/Resources/setting/addon/KubernetesSettingsAddon.py
+++ src/Resources/setting/addon/KubernetesSettingsAddon.py
@@ -2,14 +2,16 @@ from ...foundation.setting.SettingsAddon import SettingsAddon
 
 
 class KubernetesSettingsAddon(SettingsAddon):
-    __slots__ = ['api_server_url', 'api_key']
+    __slots__ = ['api_server_url', 'api_token', 'host_shared']
 
     def __init__(self):
-        self.api_server_url = "localhost"
-        self.api_key = "fruskit"
+        self.api_server_url = None
+        self.api_token = None
+        self.host_shared = True
 
     def _to_dict(self):
         return {
             'api_server_url': self.api_server_url,
-            'api_key': self.api_key
+            'api_token': self.api_token,
+            'host_shared': self.host_shared,
         }
