6ac9b02ae1e24461e923c80afde87ff28b9e15c9
Mariano Scazzariello <marianoscazzariello@gmail.com>
2020-07-23 16:05:00 +0200

    Fix k8s stdin

--- src/Resources/os/terminal/SocketTerminal.py
+++ src/Resources/os/terminal/SocketTerminal.py
@@ -11,6 +11,12 @@ class SocketTerminal(Terminal):
     def _on_close(self):
         pass
 
+    def _write_on_external_tty(self):
+        def write_on_external_tty(handle, data, error):
+            self.handler.write_stdin(data)
+
+        return write_on_external_tty
+
     def _handle_external_tty(self):
         def handle_external_tty(timer_handle):
             data = self.handler.read_stdout()
