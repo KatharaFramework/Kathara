94b79b47ea82492024190ee57641aaeaab93df17
Tommaso Caiazzi <tommasocaiazzi@gmail.com>
2020-08-11 16:41:01 +0200

    add support to docker image tag (does not work for library/<image>:<tag>) (#95)
    * add support to docker image tag (not work for library/<image>)
    
    * add a check to avoid the update of library/<image> in Dockerimage.check_update
    
    * Removal of REST calls to Dockerhub to rely on Python's Docker API only (#1)
    
    * Adjusted SHA comparison
    
    * Adjusted Kathara.py to work with Virtualenv
    
    * Working update mechanism and download of digested images
    
    * Library images can be checked with Python's Docker API
    
    * Polishing
    
    Co-authored-by: Marcel Großmann <whatever4711@gmail.com>
    Co-authored-by: Mariano Scazzariello <marianoscazzariello@gmail.com>

--- src/Resources/manager/docker/DockerImage.py
+++ src/Resources/manager/docker/DockerImage.py
@@ -1,9 +1,8 @@
 import logging
+
 from docker.errors import APIError
 
-from ...api.DockerHubApi import DockerHubApi
 from ... import utils
-from ...exceptions import HTTPConnectionError
 
 
 class DockerImage(object):
@@ -12,34 +11,41 @@ class DockerImage(object):
     def __init__(self, client):
         self.client = client
 
-    def check_local(self, image_name):
+    def get_local(self, image_name):
         return self.client.images.get(image_name)
 
+    def get_remote(self, image_name):
+        return self.client.images.get_registry_data(image_name)
+
     def pull(self, image_name):
-        print("Pulling image `%s`... This may take a while." % image_name)
-        return self.client.images.pull(image_name, tag="latest")
+        # If no tag or sha key is specified, we add "latest"
+        if (':' or '@') not in image_name:
+            image_name = "%s:latest" % image_name
 
-    def check_update(self, image_name):
-        logging.debug("Check update for %s" % image_name)
-        local_image_info = self.check_local(image_name)
+        logging.info("Pulling image `%s`... This may take a while." % image_name)
+        return self.client.images.pull(image_name)
 
+    def check_for_updates(self, image_name):
+        logging.debug("Checking updates for %s..." % image_name)
+
+        if '@' in image_name:
+            logging.debug('No need to check image digest of %s' % image_name)
+            return
+
+        local_image_info = self.get_local(image_name)
         # Image has been built locally, so there's nothing to compare.
         local_repo_digests = local_image_info.attrs["RepoDigests"]
         if not local_repo_digests:
             logging.debug("Image %s is build locally" % image_name)
             return
 
-        try:
-            remote_image_info = self.check_remote(image_name)
-            local_repo_digest = local_repo_digests[0]
-            remote_image_digest = remote_image_info["images"][0]["digest"]
-        except HTTPConnectionError:
-            logging.debug("Unable to connect to DockerHub")
-            return
+        remote_image_info = self.get_remote(image_name).attrs['Descriptor']
+        local_repo_digest = local_repo_digests[0]
+        remote_image_digest = remote_image_info["digest"]
 
         # Format is image_name@sha256, so we strip the first part.
         (_, local_image_digest) = local_repo_digest.split("@")
-
+        # We only need to update tagged images, not the ones with digests.
         if remote_image_digest != local_image_digest:
             utils.confirmation_prompt("A new version of image `%s` has been found on Docker Hub. "
                                       "Do you want to pull it?" % image_name,
@@ -50,24 +56,20 @@ class DockerImage(object):
     def check_and_pull(self, image_name):
         try:
             # Tries to get the image from the local Docker repository.
-            self.check_local(image_name)
-            self.check_update(image_name)
+            self.get_local(image_name)
+            self.check_for_updates(image_name)
         except APIError:
             # If not found, tries on Docker Hub.
             try:
                 # If the image exists on Docker Hub, pulls it.
-                self.check_remote(image_name)
+                self.get_remote(image_name)
                 self.pull(image_name)
             except ConnectionError:
-                raise ConnectionError("Image `%s` does not exists in local and not Internet connection for Docker Hub."
+                raise ConnectionError("Image `%s` does not exists in local and no Internet connection for Docker Hub."
                                       % image_name)
             except Exception:
-                raise Exception("Image `%s` does not exists neither in local nor on Docker Hub." % image_name)
+                raise Exception("Image `%s` does not exists either in local or on Docker Hub." % image_name)
 
-    def multiple_check_and_pull(self, images):
+    def check_and_pull_from_list(self, images):
         for image in images:
             self.check_and_pull(image)
-
-    @staticmethod
-    def check_remote(image_name):
-        return DockerHubApi.get_image_information(image_name)
