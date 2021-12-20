0acdd259b48b73a3950f38cf066181a1cb487a1a
tcaiazzi <tommasocaiazzi@gmail.com>
2021-12-20 15:17:16 +0100

    fix pydocs generation

--- scripts/pydoc/generate_doc.py
+++ scripts/pydoc/generate_doc.py
@@ -6,6 +6,6 @@ ignored_modules = [
     "Kathara.exceptions", "Kathara.strings", "Kathara.utils", "kathara"
 ]
 
-generate_docs(["Kathara"], src_root_path="../../src", output_path="./docs",
+generate_docs(["../../src"], src_root_path="../../src/", output_path="./docs",
               ignored_modules=ignored_modules,
               overview_file="Kathara-API-Docs.md", remove_package_prefix=False)
