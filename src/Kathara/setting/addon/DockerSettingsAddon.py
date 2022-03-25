10590b8e5e114fe9a12c5c02c2cef58ed5217bfd
tcaiazzi <tommasocaiazzi@gmail.com>
2022-03-25 11:24:08 +0100

    Add option in settings to select the image update policy (close #156)

--- src/Kathara/setting/addon/DockerSettingsAddon.py
+++ src/Kathara/setting/addon/DockerSettingsAddon.py
@@ -5,6 +5,7 @@ from ...foundation.setting.SettingsAddon import SettingsAddon
 DEFAULTS = {
     "hosthome_mount": False,
     "shared_mount": True,
+    "image_update_policy": "Prompt",
     "shared_cd": False,
     "remote_url": None,
     "cert_path": None
@@ -12,11 +13,12 @@ DEFAULTS = {
 
 
 class DockerSettingsAddon(SettingsAddon):
-    __slots__ = ['hosthome_mount', 'shared_mount', 'shared_cd', 'remote_url', 'cert_path']
+    __slots__ = ['hosthome_mount', 'shared_mount', 'image_update_policy', 'shared_cd', 'remote_url', 'cert_path']
 
     def __init__(self) -> None:
         self.hosthome_mount: bool = False
         self.shared_mount: bool = True
+        self.image_update_policy: str = 'Prompt'
         self.shared_cd: bool = False
         self.remote_url: Optional[str] = None
         self.cert_path: Optional[str] = None
@@ -25,6 +27,7 @@ class DockerSettingsAddon(SettingsAddon):
         return {
             'hosthome_mount': self.hosthome_mount,
             'shared_mount': self.shared_mount,
+            'image_update_policy': self.image_update_policy,
             'shared_cd': self.shared_cd,
             'remote_url': self.remote_url,
             'cert_path': self.cert_path
