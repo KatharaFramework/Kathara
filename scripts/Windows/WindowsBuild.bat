52dd4687d38c502ee1336f10f5abc466b2bcebcb
Mariano Scazzariello <marianoscazzariello@gmail.com>
2022-09-29 11:35:54 +0200

    Switch windows 10 build to Python3.10
    Co-authored-by: Tommaso Caiazzi <tommasocaiazzi@gmail.com>

--- scripts/Windows/WindowsBuild.bat
+++ scripts/Windows/WindowsBuild.bat
@@ -1,14 +1,12 @@
-@ECHO OFF
-
 set VENV_DIR=%cd%\venv
 
-python -m venv %VENV_DIR%
+python3.10 -m venv %VENV_DIR%
 CALL %VENV_DIR%\Scripts\activate
 
-pip install win_inet_pton
-pip install pyinstaller
-pip install -r ..\..\src\requirements.txt
-pip install pytest
+pip3.10 install win_inet_pton
+pip3.10 install pyinstaller
+pip3.10 install -r ..\..\src\requirements.txt
+pip3.10 install pytest
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 copy Assets\kathara.spec ..\..\src\
@@ -22,8 +20,8 @@ cd src\
 
 pyinstaller --distpath=./kathara.dist --workpath=./kathara.build kathara.spec
 
-python -c "for p in __import__('pathlib').Path('.').rglob('*.py[co]'): p.unlink()"
-python -c "for p in __import__('pathlib').Path('.').rglob('__pycache__'): p.rmdir()"
+python3.10 -c "for p in __import__('pathlib').Path('.').rglob('*.py[co]'): p.unlink()"
+python3.10 -c "for p in __import__('pathlib').Path('.').rglob('__pycache__'): p.rmdir()"
 
 del kathara.spec
 del app_icon.ico
